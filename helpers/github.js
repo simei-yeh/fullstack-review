e8073f0fe445b3b4b18842e7a1ba362c9eeddecd
Simei Yeh <simei.yeh@gmail.com>
2020-11-06 21:03:41 -0800

    Updated procfiles and env

--- helpers/github.js
+++ helpers/github.js
@@ -5,7 +5,7 @@ const db = require('../database/index.js');
 let getReposByUsername = (username, callback) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
-
+  let token = config.TOKEN || process.env.API_KEY;
   // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
@@ -13,7 +13,7 @@ let getReposByUsername = (username, callback) => {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${token}`
     }
   };
 
